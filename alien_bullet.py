39345f01024fa95551a9e57d6104b171806ab4c3
dizhu <dizhu210@gmail.com>
2019-06-10 01:38:15 -0400

    added an alien bullet class

--- alien_bullet.py
+++ alien_bullet.py
@@ -1,31 +1,23 @@
 import pygame
-from pygame.sprite import Sprite
+from bullet import Bullet
 
-class AlienBullet(Sprite):
-    """A class to manage bullets fired from the ship"""
+class AlienBullet(Bullet):
+    """A class to manage bullets fired from the alien"""
 
     def __init__(self, ai_settings, screen, alien):
         """Create a bullet at the ship's current position"""
-        super().__init__()
-        self.screen = screen
-        self.ai_settings = ai_settings
-        #create a bullet rect at(0,0) and then set correct position
-        self.rect = pygame.Rect(0, 0, ai_settings.alien_bullet_width, ai_settings.alien_bullet_height)
+        super().__init__(ai_settings, screen, alien)
         self.rect.centerx = alien.rect.centerx
         self.rect.bottom = alien.rect.bottom
-        #store the bullet's position as a decimal value.
-        self.y = float(self.rect.y)
-        #alien bullet image
         self.alien_bullet_color = ai_settings.alien_bullet_color
-        self.alien_bullet_speed_factor = ai_settings.alien_bullet_speed_factor
 
     def update(self):
         """move the bullet up the screen"""
         #update the decimal position of the bullet
-        self.y += self.alien_bullet_speed_factor
+        self.y += self.speed_factor
         #update the rect position
         self.rect.y = self.y
 
-    def draw_alien_bullet(self):
+    def draw_bullet(self):
         """draw the bullet to screen"""
         pygame.draw.rect(self.screen, self.alien_bullet_color, self.rect, 5)
