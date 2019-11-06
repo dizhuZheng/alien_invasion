6c567045749142b7ed7d55dc433f5008b67fc783
dizhu <dizhu210@gmail.com>
2019-11-06 00:35:45 -0500

    meteor

--- meteor.py
+++ meteor.py
@@ -19,11 +19,11 @@ class Meteor(Sprite):
     def update(self, ai_settings):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        # if self.rect.top > ai_settings.screen_height + 10 or self.rect.left < -25 or self.rect.right > ai_settings.screen_width + 20:
-        #     self.rect.x = random.randrange(ai_settings.screen_width - self.rect.width)
-        #     self.rect.y = random.randrange(-100, -40)
-        #     self.speedy = random.randrange(1, 8)
-        #     self.speedx = random.randrange(-3, 3)
+        if self.rect.top > ai_settings.screen_height + 10 or self.rect.left < -25 or self.rect.right > ai_settings.screen_width + 20:
+            self.rect.x = random.randrange(ai_settings.screen_width - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+            self.speedx = random.randrange(-3, 3)
 
     def blitme(self):
         """draw the bonus at its current location"""
